8e3c303d5a1148cbb1f77ce38de02cb486d3bbd0
waielelbashir-oss <waiel.elbashir@gmail.com>
2026-06-28 21:31:30 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-from backend.database import engine
+from backend.database import engine, Base
+import backend.models
 
 app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def home():
     return {"status": "OK", "project": "Abanos"}
+
 
 @app.get("/db-check")
 def db_check():
